987564733050a12d80fdcb1d7ade8d2de33e8ec7
Oksana Chernenko <senia@MacBook-Air-Oksana.local>
2023-07-04 16:35:41 +0300

    mkougfvn

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
-          Hello im header dkkskssk
+          wooow
         </p>
         <a
           className="App-link"
