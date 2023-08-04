9b27e53b14126701efb3328935b478db5ce2c85a
Oksana Chernenko <mirajane2207@gmail.com>
2023-08-04 10:39:46 +0300

    Added gradient border to review items

---
+++ src/components/ReviewItem/ReviewItem.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import classes from './ReviewItem.module.css'
+
+const ReviewItem = () => {
+  return (
+    <div className={classes.review__container}>
+    </div>
+  )
+}
+
+export default ReviewItem
--- src/components/Reviews/Reviews.jsx
+++ src/components/Reviews/Reviews.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import classes from './Reviews.module.css'
 import SectionTitle from '../SectionTitle/SectionTitle'
 import personImg from './reviews-person.svg'
+import ReviewItem from '../ReviewItem/ReviewItem'
 
 const Reviews = () => {
     return (
@@ -10,7 +11,17 @@ const Reviews = () => {
                 <SectionTitle>Відгуки про нас</SectionTitle>
                 <img src={personImg} alt="" />
             </div>
-            
+            <div className={classes.reviews__items_container}>
+                <ReviewItem/>
+                <ReviewItem/>
+                <ReviewItem/>
+                <ReviewItem/>
+                <ReviewItem/>
+                <ReviewItem/>
+                <ReviewItem/>
+                <ReviewItem/>
+                <ReviewItem/>
+            </div>
         </div>
     )
 }
