5aace504dae5409752a3d1e47bde384e577573fc
Oksana Chernenko <mirajane2207@gmail.com>
2023-08-29 19:56:44 +0300

    fixed tarif UI

--- src/components/TariffItem/TariffItem.jsx
+++ src/components/TariffItem/TariffItem.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import classes from './TariffItem.module.css'
-import CustomButton from '../CustomButton/CustomButton'
 
 const TariffItem = (props) => {
     return (
@@ -8,14 +7,14 @@ const TariffItem = (props) => {
             <div className={classes.tariff__title}>
                 <h3>{props.title}</h3>
                 <div className={classes.tariff__details}>
-                    <p>{props.qty}</p>
+                    <p className={classes.tariff__qty}>{props.qty}</p>
                     <h3>{props.price}</h3>
                 </div>
             </div>
             <p className={classes.tariff__desc}>
                 {props.desc}
             </p>
-            <CustomButton disabled >Придбати</CustomButton>
+            <button>Придбати</button>
         </div>
     )
 }
