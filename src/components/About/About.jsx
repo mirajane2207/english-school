5626a5b691dcdf0429b02a753426031151cde701
Oksana Chernenko <mirajane2207@gmail.com>
2023-08-29 15:38:53 +0300

    Fixed About UI

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,17 +1,19 @@
 import React from 'react'
 import classes from './About.module.css'
 import SectionTitle from '../SectionTitle/SectionTitle.jsx'
-import hand from './about-hand-img.png'
+import hand from './about-hand-img.svg'
 
 const About = () => {
     return (
         <div className={classes.about__container}>
-            <SectionTitle>Про школу</SectionTitle>
-            <div className={classes.about__info}>
-                <h3 className={classes.about__info}>Ми молода але вже успішна школа англійської</h3>
-                <p>Школа англійської мови Pengli - це команда молодих, креативних викладачів, які постійно розвиваються, вдосконалюються, а також всім серцем люблять англійську мову і знають як зробити так аби ви без жодних страждань та дискомфорту заговорили англійською незгірше Бориса Джонсона. За нашими плечами 5 років досвіду викладання, а також великий багаж цінних знань, якими ми радо ділимось з нашими студентами.</p>
+            <div className={classes.about__text}>
+                <SectionTitle>Про школу</SectionTitle>
+                <div className={classes.about__info}>
+                    <h3>Ми молода але вже успішна школа англійської</h3>
+                    <p>Школа англійської мови Pengli - це команда молодих, креативних викладачів, які постійно розвиваються, вдосконалюються, а також всім серцем люблять англійську мову і знають як зробити так аби ви без жодних страждань та дискомфорту заговорили англійською незгірше Бориса Джонсона. За нашими плечами 5 років досвіду викладання, а також великий багаж цінних знань, якими ми радо ділимось з нашими студентами.</p>
+                </div>
             </div>
-            <img src={hand} alt="" className={classes.about__img}/>
+            <img src={hand} alt="" className={classes.about__img} />
         </div>
     )
 }
