cb53ade5d84f74cbea315c3c4a716a82a0420a79
Oksana Chernenko <mirajane2207@gmail.com>
2023-09-27 17:54:47 +0300

    Implemented smooth open

--- src/components/ParentsAskItem/ParentsAskItem.jsx
+++ src/components/ParentsAskItem/ParentsAskItem.jsx
@@ -1,19 +1,28 @@
-import { React, useState } from 'react'
+import { React, useRef, useState } from 'react'
 import classes from './ParentsAskItem.module.css'
 
 const ParentsAskItem = (props) => {
     const [state, setState] = useState('closed');
+    const [visible, setVisible] = useState(true)
 
     function changeState() {
         setState(previous => previous === 'closed' ? 'opened' : 'closed')
+        setVisible(!visible)
+    }
+
+    const rootClasses = [classes.item__opened_container]
+    if(visible) {
+        rootClasses.push(classes.active)
     }
 
     return (
         <div className={classes.item__container}>
-            {state === 'closed'
-                ? <div className={classes.item__closed_container}> <h3>{props.title}</h3>
+            {/* {state === 'closed'
+                ? <div id='content' className={classes.item__closed_container}> <h3>{props.title}</h3>
                     <button onClick={changeState} className={classes.btn_open}></button></div>
-                : <div className={classes.item__opened_container}>
+                : */}
+                
+                <div className={rootClasses.join(' ')} >
                     <div className={classes.item__opened_content_container}>
                         <h3>{props.title}</h3>
                         <button onClick={changeState} className={classes.btn_close}></button>
@@ -21,7 +30,8 @@ const ParentsAskItem = (props) => {
                     <hr />
                     <p>{props.text}</p>
                 </div>
-            }
+            
+            {/* } */}
 
 
         </div>
