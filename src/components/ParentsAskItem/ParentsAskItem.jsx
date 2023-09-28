c0468fd4631a50210b054f68c19351c84a52927c
Oksana Chernenko <mirajane2207@gmail.com>
2023-09-28 20:31:50 +0300

    Removed unsed classes + implemented smooth open for mobile

--- src/components/ParentsAskItem/ParentsAskItem.jsx
+++ src/components/ParentsAskItem/ParentsAskItem.jsx
@@ -17,23 +17,14 @@ const ParentsAskItem = (props) => {
 
     return (
         <div className={classes.item__container}>
-            {/* {state === 'closed'
-                ? <div id='content' className={classes.item__closed_container}> <h3>{props.title}</h3>
-                    <button onClick={changeState} className={classes.btn_open}></button></div>
-                : */}
-                
                 <div className={rootClasses.join(' ')} >
                     <div className={classes.item__opened_content_container}>
                         <h3>{props.title}</h3>
-                        <button onClick={changeState} className={classes.btn_close}></button>
+                        <button onClick={changeState}></button>
                     </div>
                     <hr />
                     <p>{props.text}</p>
                 </div>
-            
-            {/* } */}
-
-
         </div>
     )
 }
