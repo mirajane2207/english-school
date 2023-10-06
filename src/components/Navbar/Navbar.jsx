e7f96d0b6fd7665dccb914f3f0c848b2292ef65c
Oksana Chernenko <mirajane2207@gmail.com>
2023-10-06 21:53:12 +0300

    Implemented responsive navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, { useMemo, useState } from 'react';
 import "./Navbar.css";
 import logo from './logo.svg';
 import ua from './ua.svg';
 
 const Navbar = (props) => {
+
+    const [isNavbarExpanded, setIsNavbarExpanded] = useState(false)
+    const [isMobile, setIsMobile] = useState(false)
+
+    window.onresize = () => {
+        if (window.screen.availWidth < 520) {
+            setIsMobile(true)
+        } else {
+            setIsMobile(false)
+        }
+    }
 
     function setAdult() {
         props.changeMode('adult')
@@ -16,31 +27,63 @@ const Navbar = (props) => {
     return (
         <nav>
             <div className='left'>
-                <img src={logo} alt="Logo" className='logo'/>
+                <img src={logo} alt="Logo" className='logo' />
                 <a href="#" className={props.mode === 'adult' ? 'header__mode_active' : 'header__mode'} onClick={setAdult}>Для дорослих</a>
-                <a href="#" className={props.mode === 'children' ? 'header__mode_active' : 'header__mode'}  onClick={setChildren}>Для дітей</a>
+                <a href="#" className={props.mode === 'children' ? 'header__mode_active' : 'header__mode'} onClick={setChildren}>Для дітей</a>
             </div>
             <div className='navbar'>
-                <ul>
-                    <li><a onClick={() => props.scrollTo("#about")}>Про школу</a></li>
-                    <li><a onClick={() => props.scrollTo('#tariffs')}>Тарифи</a></li>
-                    {
-                        props.mode === 'adult'
-                        ?      <li><a onClick={() => props.scrollTo('#reviews')}>Відгуки</a></li>
-                        :    <li><a onClick={() => props.scrollTo('#parentsAsk')}>Батьки питають</a></li>
-                    }
-                    <li>
-                        <img src={ua} alt="" className='uaImg'/>
-                        {/* <select name="lng">
+                {
+                    !isMobile 
+
+                        ? < ul >
+                            <li><a onClick={() => props.scrollTo("#about")}>Про школу</a></li>
+                            <li><a onClick={() => props.scrollTo('#tariffs')}>Тарифи</a></li>
+                            {
+                                props.mode === 'adult'
+                                    ? <li><a onClick={() => props.scrollTo('#reviews')}>Відгуки</a></li>
+                                    : <li><a onClick={() => props.scrollTo('#parentsAsk')}>Батьки питають</a></li>
+                            }
+                            <li>
+                                <img src={ua} alt="" className='uaImg' />
+                                {/* <select name="lng">
                             <option value="ua">Ua</option>
                             <option value="en">En</option>
                             <option value="ru">Ru</option>
                         </select> */}
-                    </li>
-                    <li><button className='contact-us-btn' onClick={props.contactUs}>Зв'язатися з нами</button></li>
-                </ul>
+                            </li>
+                            <li><button className='contact-us-btn' onClick={props.contactUs}>Зв'язатися з нами</button></li>
+                        </ul>
+                        : <div className='humburger_container'>
+                            <img src={ua} alt="" className='uaImg' />
+                            {
+                                isNavbarExpanded
+                                    ? <div className='navbar-mobile expanded'>
+                                        < ul >
+                                            <li>
+                                                <div className='navbar-head'>
+                                                    <a><img src={logo} alt="Logo" className='logo' /></a>
+                                                    <button className='close-btn' onClick={() => setIsNavbarExpanded(false)}></button>
+                                                </div>
+                                            </li>
+                                            <li><a className='navbar-item' onClick={() => props.scrollTo("#about")}>Про школу</a></li>
+                                            <li><a className='navbar-item' onClick={() => props.scrollTo('#tariffs')}>Тарифи</a></li>
+                                            {
+                                                props.mode === 'adult'
+                                                    ? <li><a onClick={() => props.scrollTo('#reviews')}>Відгуки</a></li>
+                                                    : <li><a onClick={() => props.scrollTo('#parentsAsk')}>Батьки питають</a></li>
+                                            }
+                                            <li><button className='navbar-item-btn' onClick={props.contactUs}>Зв'язатися з нами</button></li>
+                                        </ul>
+                                    </div>
+                                    :
+                                    <div className='navbar-mobile'>
+                                    </div>
+                            }
+                            <button className='humburger' onClick={() => setIsNavbarExpanded(true)}></button>
+                        </div>
+                }
             </div>
-        </nav>
+        </nav >
     )
 }
 
