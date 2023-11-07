24960d25c518c2b09edf07045515029abe4d65ef
Oksana Chernenko <mirajane2207@gmail.com>
2023-11-07 23:37:39 +0200

    Update adaptive: about

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,16 +1,21 @@
 import React from 'react'
-import SectionTitle from '../SectionTitle/SectionTitle.jsx'
-import classes from './About.module.css'
-import adultMobImg from './about-adult-mobile-img.svg'
-import childrenMobImg from './about-children-mobile-img.svg'
+import classes from './About2.module.css'
 
 const About = (props) => {
     return (
-        <div className={classes.about__container} id='about'>
-            <div className={classes.about__text}>
+        <div className={classes.section__container} id='about'>
+            <h2 className={classes.section__title}>Про школу</h2>
+            <div className={classes.about__text_container}>
+                <h3 className={classes.about__title}>{props.title}</h3>
+                <p className={classes.about__text}>{props.text}</p>
+            </div>
+            <div className={props.mode === 'adult' ? classes.about__adult_img : classes.about__children_img}></div>
+
+
+            {/* <div className={classes.about__text}>
                 <div className={classes.about__info}>
                     <div className={classes.about__head_container}>
-                        <div style={{ display: 'flex', flexDirection: 'column', gap: '24px'}}>
+                        <div style={{ display: 'flex', flexDirection: 'column', gap: '24px' }}>
                             <SectionTitle>Про школу</SectionTitle>
                             <h3 className={props.mode === 'adult' ? classes.about__h3_adult : classes.about__h3_children}>{props.title}</h3>
 
@@ -31,14 +36,16 @@ const About = (props) => {
                     </div>
                 </div>
             </div>
-            <div className={props.mode === 'adult' ? classes.about__adult_img : classes.about__children_img}>
 
-                </div>
+            
 
             {props.mode === 'children'
-                ? <div className={classes.about__children_rect}></div>
-                : <div style={{position: 'absolute'}}></div>
-            }
+                ? <div className={classes.about__children_rect}>
+                    <div className={classes.about__children_img}></div>
+                </div>
+                : <div className={classes.about__adult_img}></div>
+
+            } */}
         </div>
     )
 }
