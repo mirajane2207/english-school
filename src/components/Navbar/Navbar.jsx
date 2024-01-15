e78356c51f2f20032757c51a45468b29f7f02083
Oksana Chernenko <mirajane2207@gmail.com>
2024-01-15 09:54:38 +0200

    cleaned code

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -7,17 +7,17 @@ const Navbar = (props) => {
     const [isNavbarExpanded, setIsNavbarExpanded] = useState(false)
     const [windowWidth, setWindowWidth] = useState(window.innerWidth);
 
-  useEffect(() => {
-    const handleResize = () => {
-      setWindowWidth(window.innerWidth);
-    };
+    useEffect(() => {
+        const handleResize = () => {
+            setWindowWidth(window.innerWidth);
+        };
 
-    window.addEventListener('resize', handleResize);
+        window.addEventListener('resize', handleResize);
 
-    return () => {
-      window.removeEventListener('resize', handleResize);
-    };
-  }, []);
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, []);
 
     function setAdult() {
         props.changeMode('adult')
@@ -35,19 +35,19 @@ const Navbar = (props) => {
     return (
         <nav className='menu'>
             <div className='left'>
-                <img src={logo} alt="Logo" className='logo' />
-                <a href="/#" className={props.mode === 'adult' ? 'header__mode_active' : 'header__mode'} onClick={setAdult}>Для дорослих</a>
+                <a href="/"><img src={logo} alt="Logo" className='logo' /></a>
+                <a href="/" className={props.mode === 'adult' ? 'header__mode_active' : 'header__mode'} onClick={setAdult}>Для дорослих</a>
                 <a href="/#" className={props.mode === 'children' ? 'header__mode_active' : 'header__mode'} onClick={setChildren}>Для дітей</a>
             </div>
             <div className='navbar'>
-                { windowWidth > 820
+                {windowWidth > 820
                     ? < ul >
-                        <li><a href="/#" onClick={() => props.scrollTo("#about")}>Про школу</a></li>
-                        <li><a href="/#" onClick={() => props.scrollTo('#tariffs')}>Тарифи</a></li>
+                        <li onClick={() => props.scrollTo("#about")}>Про школу</li>
+                        <li onClick={() => props.scrollTo('#tariffs')}>Тарифи</li>
                         {
                             props.mode === 'adult'
-                                ? <li><a href="/#" onClick={() => props.scrollTo('#reviews')}>Відгуки</a></li>
-                                : <li><a href="/#" onClick={() => props.scrollTo('#parentsAsk')}>Батьки питають</a></li>
+                                ? <li onClick={() => props.scrollTo('#reviews')}>Відгуки</li>
+                                : <li onClick={() => props.scrollTo('#parentsAsk')}>Батьки питають</li>
                         }
                         <li><button className='contact-us-btn' onClick={props.contactUs}>Зв'язатися з нами</button></li>
                     </ul>
@@ -58,16 +58,16 @@ const Navbar = (props) => {
                                     < ul >
                                         <li>
                                             <div className='navbar-head'>
-                                                <a href='/#'><img src={logo} alt="Logo" className='logo' /></a>
+                                                <a href='/'><img src={logo} alt="Logo" className='logo' /></a>
                                                 <button className='close-btn' onClick={() => setIsNavbarExpanded(false)}></button>
                                             </div>
                                         </li>
-                                        <li><a href="/#" className='navbar-item' onClick={() => scrollMobile('#about')}>Про школу</a></li>
-                                        <li><a href="/#" className='navbar-item' onClick={() => scrollMobile('#tariffs')}>Тарифи</a></li>
+                                        <li><a href="/" className='navbar-item' onClick={() => scrollMobile('#about')}>Про школу</a></li>
+                                        <li><a href="/" className='navbar-item' onClick={() => scrollMobile('#tariffs')}>Тарифи</a></li>
                                         {
                                             props.mode === 'adult'
-                                                ? <li><a href="/#" onClick={() => scrollMobile('#reviews')}>Відгуки</a></li>
-                                                : <li><a href="/#" onClick={() => scrollMobile('#parentsAsk')}>Батьки питають</a></li>
+                                                ? <li><a href="/" onClick={() => scrollMobile('#reviews')}>Відгуки</a></li>
+                                                : <li><a href="/" onClick={() => scrollMobile('#parentsAsk')}>Батьки питають</a></li>
                                         }
                                         <li><button className='navbar-item-btn' onClick={props.contactUs}>Зв'язатися з нами</button></li>
                                     </ul>
